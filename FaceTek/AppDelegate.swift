91852ed821c98a2b54cd3f92b313658b68db7e7c
suresh <69379806+sureshbandaruios@users.noreply.github.com>
2020-08-13 20:57:24 +0530

    Update AppDelegate.swift
    Changed name

--- FaceTek/AppDelegate.swift
+++ FaceTek/AppDelegate.swift
@@ -19,6 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     //fdgfgdgdggdgdgfg
 	
 	//Manjunath
+    
+    //suresh Bandaru
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
