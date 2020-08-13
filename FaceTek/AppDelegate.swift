5ea7603f03e606a8cc55285b7c1331c65b1a0207
Manjunath Gudisi <manjunath.gudisi@gmail.com>
2020-08-13 20:48:47 +0530

    Testing Push and Pull
    Testing Push and Pull

--- FaceTek/AppDelegate.swift
+++ FaceTek/AppDelegate.swift
@@ -17,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 //abc.....
     //fdgfgdgdggdgdgfg
+	
+	//Manjunath
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
