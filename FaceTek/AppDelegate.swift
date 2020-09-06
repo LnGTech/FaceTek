3a53fc57a6c79a82b9ef62c28f8974149151651a
suresh <69379806+sureshbandaruios@users.noreply.github.com>
2020-09-06 17:54:57 +0530

    changed Appdelegate
    changed Appdelegate

--- FaceTek/AppDelegate.swift
+++ FaceTek/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 //sdfsfs
     //sdfsfsd
     //sdsfsfds
+    //suresh bandaru
 
     var window: UIWindow?
     var recognitionViewController: RecognitionViewController?
