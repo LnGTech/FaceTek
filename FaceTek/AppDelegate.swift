aa8c78dcbe2fd84fcabf5485b965d06fa63b8ca8
suresh <69379806+sureshbandaruios@users.noreply.github.com>
2020-09-06 16:15:21 +0530

    Add changes
    Add changes

--- FaceTek/AppDelegate.swift
+++ FaceTek/AppDelegate.swift
@@ -13,7 +13,8 @@ import GoogleMaps
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 //updates
-//suresh bandaru
+//sdfsfs
+
     var window: UIWindow?
     var recognitionViewController: RecognitionViewController?
 
