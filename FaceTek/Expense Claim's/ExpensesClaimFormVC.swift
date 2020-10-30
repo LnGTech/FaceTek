0656cd137b047c92915ff75c5e19d910ed250b6d
suresh <69379806+sureshbandaruios@users.noreply.github.com>
2020-10-30 16:51:38 +0530

    Working On Expense Claim design and Auto Layout's
    Working On Expense Claim design and Auto Layout's

--- FaceTek/Expense Claim's/ExpensesClaimFormVC.swift
+++ FaceTek/Expense Claim's/ExpensesClaimFormVC.swift
@@ -1,5 +1,5 @@
 //
-//  ExpenseClaimVC.swift
+//  ExpensesClaimFormVC.swift
 //  FaceTek
 //
 //  Created by sureshbabu bandaru on 10/30/20.
@@ -8,39 +8,22 @@
 
 import UIKit
 
-class ExpenseClaimVC: UIViewController {
+class ExpensesClaimFormVC: UIViewController {
 
-	@IBOutlet weak var DateView: UIView!
-	override func viewDidLoad() {
+    override func viewDidLoad() {
         super.viewDidLoad()
 		
 		let statusBarView = UIView(frame: UIApplication.shared.statusBarFrame)
 		statusBarView.backgroundColor = #colorLiteral(red: 0.05490196078, green: 0.2980392157, blue: 0.5450980392, alpha: 1)
 		view.addSubview(statusBarView)
-		
-		DateView.layer.borderWidth = 1
-		DateView.layer.borderColor = UIColor.lightGray.cgColor
-
 
         // Do any additional setup after loading the view.
     }
     
-	
-	@IBAction func Btnclk(_ sender: Any) {
-		let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
-		
-		
-		let ExpensesClaimFormVC = storyBoard.instantiateViewController(withIdentifier: "ExpensesClaimFormVC") as! ExpensesClaimFormVC
-		self.present(ExpensesClaimFormVC, animated:true, completion:nil)
-        
-		
-		
-	}
 	@IBAction func BackBtnclk(_ sender: Any) {
 		self.presentingViewController?.dismiss(animated: false, completion: nil)
 
 
 	}
-	
 	
 }
